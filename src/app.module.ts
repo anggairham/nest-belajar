7f37c8f78ca593e87951a0060fc03a7840d0900c
Anggairham <anggairham08@gmail.com>
2024-08-25 19:41:03 +0700

    global provider "Added RoleGuard as global guard in AppModule and removed UseGuards decorator from UserController"

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,8 @@ import { ValidationModule } from './validation/validation.module';
 import * as winston from 'winston';
 import { LogMiddleware } from './log/log.middleware';
 import { AuthMiddleware } from './auth/auth.middleware';
+import { APP_GUARD } from '@nestjs/core';
+import { RoleGuard } from './role/role.guard';
 
 @Module({
   imports: [
@@ -31,7 +33,13 @@ import { AuthMiddleware } from './auth/auth.middleware';
     ValidationModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: RoleGuard,
+    },
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
