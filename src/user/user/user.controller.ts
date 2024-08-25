dba8d164ddcdc5f7f1fc127d8287e2812f5f5ba4
Anggairham <anggairham08@gmail.com>
2024-08-25 19:38:18 +0700

    reflector "Refactor RoleGuard to use Reflector and Roles decorator, update UserController to use Roles decorator"

--- src/user/user/user.controller.ts
+++ src/user/user/user.controller.ts
@@ -36,7 +36,9 @@ import { ValidationPipe } from 'src/validation/validation.pipe';
 import { TimeInterceptor } from 'src/time/time.interceptor';
 import { Auth } from 'src/auth/auth.decorator';
 import { RoleGuard } from 'src/role/role.guard';
+import { Roles } from 'src/role/roles.decorator';
 
+@UseGuards(RoleGuard)
 @Controller('/api/users')
 export class UserController {
   /* 
@@ -59,7 +61,10 @@ export class UserController {
   ) {}
 
   @Get('/current')
-  @UseGuards(new RoleGuard(['admin', 'operator']))
+  // @UseGuards(new RoleGuard(['admin', 'operator']))
+  // singleton Reflector
+  // @UseGuards(RoleGuard)
+  @Roles(['admin', 'operator'])
   current(@Auth() user: User): Record<string, any> {
     return {
       data: `Hello ${user.first_name} ${user.last_name}`,
